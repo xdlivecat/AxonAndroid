570acfa82cecf5ffd3954bdce9d1faba1d326bfa
xdlivecat <xdliverblx@hotmail.com>
2024-01-22 10:35:45 +0100

    Initial commit

---
+++ app/src/main/java/com/xdliverblx/axon/MainActivity.java
@@ -0,0 +1,67 @@
+package com.xdliverblx.axon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Display;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+import com.xdliverblx.axon.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://paulovm.com/");
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setSaveFormData(true);
+        webView.getSettings().setAllowContentAccess(true);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.getSettings().setAllowFileAccessFromFileURLs(true);
+        webView.getSettings().setAllowUniversalAccessFromFileURLs(true);
+        webView.getSettings().setSupportZoom(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.setClickable(true);
+        webView.setWebChromeClient(new WebChromeClient());
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+    }
+
+        public class mywebClient extends WebViewClient {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view,url,favicon);
+            }
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        }
+        @Override
+        public void onBackPressed() {
+            WebView mywebView=(WebView) findViewById(R.id.webview);
+            if (mywebView.canGoBack()) {
+                mywebView.goBack();
+            } else {
+                super.onBackPressed();
+            }
+        }
+}
